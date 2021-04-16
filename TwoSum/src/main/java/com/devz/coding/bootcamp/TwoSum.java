83abdc61d46f7f94f4aee90e99534b83c255f30f
Luis <luisayala.c@gmail.com>
2021-04-15 20:36:29 -0500

    Add a map for storing the nums[i] value and its index in order to avoid a search on the list

--- TwoSum/src/main/java/com/devz/coding/bootcamp/TwoSum.java
+++ TwoSum/src/main/java/com/devz/coding/bootcamp/TwoSum.java
@@ -7,19 +7,20 @@ public class TwoSum {
 
     public static void main(String[] args) {
         int[] nums = new int[] {2, -1, 1, 5 };
-        System.out.println(Arrays.toString(twoSum(nums, 0)));
+        System.out.println(Arrays.toString(twoSum(nums, 0))); //[1, 2]
     }
 
     public static int[] twoSum(final int[] nums, final int target) {
-        final List<Integer> numbers = Arrays.stream(nums).boxed().collect(Collectors.toList());
+        Map<Integer, Integer> a = new HashMap<>();
 
         for(int index = 0; index < nums.length; index++) {
-            int found = Collections.binarySearch(numbers, (target - numbers.get(index)));
-            if( found == -1 || found == index)
-                continue;
+            int complement = target - nums[index];
 
-            return new int[] { index, found };
+            if (a.containsKey(complement)) {
+                return new int[] { a.get(complement) , index };
+            }
 
+            a.put(nums[index], index);
         }
 
         return new int[0];
